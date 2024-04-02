8be01867f66f96f6444cbb0989d1a807c9c4de31
sky line <32512284+linesky@users.noreply.github.com>
2024-04-02 21:04:12 +0100

    Add files via upload

--- cgi-bin/run.sh
+++ cgi-bin/run.sh
@@ -1,11 +1,12 @@
-#!/bin/bash
-printf "Content-type:text/txt\r\n\r\n"
+#!/usr/bin/bash
 d=$(date)
+QUERY_STRING=$1
 list2=$($QUERY_STRING)
-echo $QUERY_STRING
-echo $d
-echo $list2
-
+echo '<html><body bgcolor="blue">'
+echo $QUERY_STRING '<br>'
+echo $d '<br>'
+echo $list2 | sed 's/ /<br>/g'
+echo '</body></html>'
 
 
 
